20c82ae14bcdf26cb65c4a43fb0bc6e91bd42038
sergey-v-sedov <sergey.v.sedov@gmail.com>
2023-07-13 21:39:49 +0300

    next commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -24,5 +22,44 @@ public class Main {
                 .max(Comparator.comparingInt(String::length)).orElse("");
 
         System.out.println(longestWord);
+
+        //======================================
+
+        List<List<String>> countries = Arrays.asList(
+                Arrays.asList("Moscow", "St.Pet"),
+                Arrays.asList("Berlin", "Frankfurt"),
+                Arrays.asList("New York", "Moscow")
+        );
+
+        long count = countries.stream().flatMap((country)-> country.stream()).count();
+        System.out.println(count);
+
+        Set<String> cities = countries.stream().flatMap((country)-> country.stream()).collect(Collectors.toSet());
+        System.out.println(cities);
+
+        //======================================
+
+        List<String> words = Arrays.asList("dsdfdsd", "dfsdfdsfds", "dfsd", "fgd", "d");
+        Map<String, Integer> wordsLength = words.stream().collect(Collectors.toMap(word->word, word->word.length()));
+        System.out.println(wordsLength);
+
+        //======================================
+
+        String allWords = words.stream().filter(word->word.length()>3).collect(Collectors.collectingAndThen(Collectors.toList(), list -> list.stream().collect(Collectors.joining("*"))));
+        System.out.println(allWords);
+
+        String value = words.toString();
+        System.out.println(value);
+
+        //======================================
+
+        String wordsWithSeparator = words.stream().collect(Collectors.joining(";"));
+        System.out.println(wordsWithSeparator);
+
+        //======================================
+
+        List<String> fruits = Arrays.asList("Apple", "Banana", "Orange", "Avocado");
+        Map<Character, Long> countByLength = fruits.stream().collect(Collectors.groupingBy((fruit)->fruit.charAt(0), Collectors.counting()));
+        System.out.println(countByLength);
     }
 }
