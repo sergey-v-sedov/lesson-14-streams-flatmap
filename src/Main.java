d93178cffd5b6d0eb6c185a4234a2abdf64d22a6
sergey-v-sedov <sergey.v.sedov@gmail.com>
2023-07-13 20:36:59 +0300

    next commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        ;
+
+        // Дан список строк, представляющих предложения. Используя Stream API
+        // отсортируйте предложения по количеству слов в каждом предложении в порядке убывания.
+
+        List<String> text = Arrays.asList(
+                "sdfsdf dfgsdfg dfgsdfg dfgdfsgdsf dfgsdfgsdf dsfgdfsgsdf",
+                "sdfsdf dfgsdfg dfgsdfg dfgdfsgdsf dfgsdfgsdf",
+                "sdfsdf dfgsdfg dfgsdfg dfgdfsgdsffgdfgdfgdfgdf dfgsdfgsdf dsfgdfsgsdf dfsdfdf",
+                "sdfsdf dfgsdfg dfgsdfg dfgdfsgdsf"
+        );
+
+        List<String> sortedText = text.stream().sorted(Comparator.comparingInt(sentence -> sentence.split(" ").length)).sorted(Comparator.reverseOrder()).toList();
+        System.out.println(sortedText);
+
+        String longestWord = text.stream()
+                .flatMap(sentence -> Arrays.stream(sentence.split(" ")))
+                .max(Comparator.comparingInt(String::length)).orElse("");
+
+        System.out.println(longestWord);
     }
 }
